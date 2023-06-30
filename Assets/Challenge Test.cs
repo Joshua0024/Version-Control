20e08da5aeabc6711873cb187992a22380130e00
Joshua Daniels <JoshuaDaniels086@gmail.com>
2023-06-29 21:12:53 -0700

    Please enter the commit message for your changes. Lines starting  '#' will be ignored, and an empty message aborts the commit.

--- Assets/Challenge Test.cs
+++ Assets/Challenge Test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChallengeTest : MonoBehaviour
+public class ChallegeTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
